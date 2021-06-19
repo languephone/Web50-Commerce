6872e7b4358265f89b8eff49c9bbb228af83511a
languephone <73309519+languephone@users.noreply.github.com>
2021-06-19 17:09:49 +0100

    Added seller to Listings model

--- auctions/models.py
+++ auctions/models.py
@@ -12,6 +12,7 @@ class Listings(models.Model):
     starting_bid = models.DecimalField(decimal_places=2, max_digits=9)
     image = models.URLField()
     category = models.CharField(max_length=64)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller_listings")
 
     def __str__(self):
         return f"{self.title}: £{self.starting_bid}"
