9c5d6a0683c40c3bb91c5b6178d4fe364cbc19d6
languephone <73309519+languephone@users.noreply.github.com>
2021-06-20 17:18:10 +0100

    Update admin interface

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import User, Listings, Bids, Comments, Watchlist
+
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+	list_display = ("username", "email", "id")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Listings)
+admin.site.register(Bids)
+admin.site.register(Comments)
+admin.site.register(Watchlist)
