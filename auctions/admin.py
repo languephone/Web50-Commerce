d1aafbe30eb484a04077bd48f890a3e74630887d
languephone <73309519+languephone@users.noreply.github.com>
2021-07-02 17:39:47 +0100

    Add BidAdmin class

--- auctions/admin.py
+++ auctions/admin.py
@@ -6,8 +6,11 @@ from .models import User, Listing, Bid, Comment, Watchlist
 class UserAdmin(admin.ModelAdmin):
 	list_display = ("username", "email", "id")
 
+class BidAdmin(admin.ModelAdmin):
+	list_display = ("listing", "bid_amount", "bidder")
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Listing)
-admin.site.register(Bid)
+admin.site.register(Bid, BidAdmin)
 admin.site.register(Comment)
 admin.site.register(Watchlist)
